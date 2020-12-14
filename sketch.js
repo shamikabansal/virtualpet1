9768b7b2b3db8e43dbddb8496d938020282c62ba
shamikabansal <71285046+shamikabansal@users.noreply.github.com>
2020-12-14 20:32:50 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var dog, happydog;
+var database; 
+var foodS, foodStock; 
+var dogimg; 
+
+function preload()
+{
+  dogimg = loadImage("images/dogimg.png");
+ happydog= loadImage("images/dogimg1.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
